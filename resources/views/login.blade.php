2293e10c1689fe4b30358640a688b3f98431e498
dealovaedelweiss-debug <ppkdjwebdealovaedelweiss@gmail.com>
2026-08-13 09:59:55 +0700

    initial commit

---
+++ resources/views/login.blade.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Login - Point Of Sales </title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+</head>
+<body>
+    <div class="login-wrapper mt-5">
+        <div class="container">
+            {{-- row tidak mempunyai ukuran
+            sedangkan container mempunyai ukuran
+            row untuk menggaturr --}}
+            <div class="row d-flex align-items-center justify-content-center">
+                <div class="col-lg-6">
+                    <div class="card shadow-lg rounded-4">
+                        <div class="card-body p-4 p-md-5">
+                            <h3 class="text-center mb-5">Login - Point Of Sales</h3>
+                            <form action="{{ route('actionlogin') }}" method="post">
+                                @csrf
+                                {{-- csrf adalah token --}}
+                                <div class="mb-3">
+                                    <label for="" class="form-label fw-semibold">Email</label>
+                                    <input type="email" name="email" class="form-control" placeholder="Enter Your Email" required>
+                                </div>
+                                <div class="mb-3">
+                                    <label for="" class="form-label fw-semibold">Password</label>
+                                    <input type="password" name="password" class="form-control" placeholder="Enter Your Password" required>
+                                </div>
+                                <button class="btn btn-primary w-100 fw-semibold" type="submit">
+                                    Sign in
+                                </button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous">
+    </script>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,35 +1,33 @@
 <?php
 
 use App\Http\Controllers\BelajarController;
+use App\Http\Controllers\LoginController;
 use App\Http\Controllers\PesertaController;
 use App\Http\Controllers\RoleController;
-use Illuminate\Support\Facades\App;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-//get: lihat dan baca
-//post: mengirim data dari form, aksinya insert
-//put: mengirim data dari form, aksinya update
-//delete: mengirim data dari form, aksinya delete
-//patch: mengirim data dari form, aksinya update
+Route::get('/', [LoginController::class, 'login']);
+Route::post('actionlogin', [LoginController::class, 'actionlogin'])->name('actionlogin');
+// get: lihat dan baca
+// post: mengirim data dari form, aksinya insert
+// put: mengirim data dari form, aksinya update
+// delete: mengirim data dari form, aksinya delete
+// patch: mengirim data dari form, aksinya update
 Route::get('salam', [BelajarController::class, 'greeting']);
 Route::get('hitung-tambah', [BelajarController::class, 'tambah'])->name('tambah');
 
 Route::get('hitung-kurang', [BelajarController::class, 'indexKurang'])->name('kurang');
-Route::post('action-kurang', [BelajarController::class, 'kurang'])->name("action-kurang");
+Route::post('action-kurang', [BelajarController::class, 'kurang'])->name('action-kurang');
 
 Route::get('hitung-kali', [BelajarController::class, 'indexKali'])->name('kali');
-Route::post('action-kali', [BelajarController::class, 'kali'])->name("action-kali");
+Route::post('action-kali', [BelajarController::class, 'kali'])->name('action-kali');
 
 Route::get('hitung-bagi', [BelajarController::class, 'indexBagi'])->name('bagi');
-Route::post('action-bagi', [BelajarController::class, 'bagi'])->name("action-bagi");
+Route::post('action-bagi', [BelajarController::class, 'bagi'])->name('action-bagi');
 
 Route::get('counting', [BelajarController::class, 'index'])->name('counting');
 
-//peserta crud
+// peserta crud
 Route::get('peserta', [PesertaController::class, 'index'])->name('peserta');
 Route::get('peserta/create', [PesertaController::class, 'create'])->name('peserta-create');
 Route::post('peserta/create', [PesertaController::class, 'store'])->name('peserta-store');
@@ -37,6 +35,5 @@ Route::get('peserta/edit/{id}', [PesertaController::class, 'edit'])->name('peser
 Route::put('peserta/edit/{id}', [PesertaController::class, 'update'])->name('peserta-update');
 Route::delete('peserta/delete/{id}', [PesertaController::class, 'delete'])->name('peserta-delete');
 
-
-//role crud
+// role crud
 Route::resource('role', RoleController::class);
