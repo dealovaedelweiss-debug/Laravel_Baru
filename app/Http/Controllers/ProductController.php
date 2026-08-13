d4787d043a842dca60e8472a89e8310dad70d145
dealovaedelweiss-debug <ppkdjwebdealovaedelweiss@gmail.com>
2026-08-13 15:37:04 +0700

    ada yang baru

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Category;
 use App\Models\Product;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 
 class ProductController extends Controller
 {
@@ -61,17 +62,38 @@ class ProductController extends Controller
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(string $id)
+    public function edit(Request $request, string $id)
     {
-        //
+
+        $title = '  Edit Product';
+        $categories = Category::get();
+        $edit = Product::findOrFail($id);
+
+        return view('product.edit', compact('title', 'categories', 'edit'));
+
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
-    {
-        //
+    public function update(Request $request, Product $product
+
+    ) {
+        $data = [
+            'name' => $request->name,
+            'category_id' => $request->category_id,
+            'price' => $request->price,
+            'description' => $request->description,
+        ];
+        if ($request->hasFile('photo')) {
+            if ($product->photo) {
+                Storage::disk('public')->delete($product->photo);
+            }
+            $data['photo'] = $request->file('photo')->store('products', 'public');
+        }
+        $product->update($data);
+
+        return redirect()->to('product')->with('success', 'Update Product Success');
     }
 
     /**
--- resources/views/product/edit.blade.php
+++ resources/views/product/edit.blade.php
@@ -1,20 +1,34 @@
 @extends('app')
 @section('content')
-    <form action="{{ route('category.update', $edit->id) }}" method="post">
+    <form action="{{ route('product.update', $edit->id) }}" method="post" enctype="multipart/form-data">
         @csrf
         @method('PUT')
         <div class="mb-3">
-            <label for="" class="form-label">Edit Name</label>
-            <input type="text" class="form-control" name="name" value="{{ $edit->name }}">
+            <label for="" class="form-label">Category</label>
+            <select name="category_id" id="" class="form-control">
+                <option value="">Select One</option>
+                @foreach ( $categories as $category )
+                    <option {{ $edit->id == $category->id ? 'selected'  : '' }}
+                    value="{{ $category->id }}">{{$category->name}}</option>
+                @endforeach
+            </select>
         </div>
         <div class="mb-3">
-            <div class="form-check form-switch">
-                <input class="form-check-input" type="checkbox" role="switch" id="is_active" name="is_active"
-                    {{ $edit->is_active == 1 ? 'checked' : '' }}>
-                <label class="form-check-label" for="is_active">Active</label>
-            </div>
+            <label for="" class="form-label">Name</label>
+            <input type="text" class="form-control" name="name">
         </div>
-
+        <div class="mb-3">
+            <label for="" class="form-label">Price</label>
+            <input type="number" class="form-control" name="price">
+        </div>
+        <div class="mb-3">
+            <label for="" class="form-label">Photo</label>
+            <input type="file" class="form-control" name="photo">
+        </div>
+        <div class="mb-3">
+            <label for="" class="form-label">Description</label>
+            <textarea name="description" id="" cols="30" rows="10" class="form-control">{{$edit->description}}</textarea>
+        </div>
         <div class="mb-3">
             <button class="btn btn-primary" type="submit">Simpan</button>
         </div>
